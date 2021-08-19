ef9899beae48c5298784235cb3c48d7822df2bf3
norjamillekasan <88083841+norjamillekasan@users.noreply.github.com>
2021-08-19 18:51:57 +0800

    z50

--- resources/views/livewire/admin/manage-user.blade.php
+++ resources/views/livewire/admin/manage-user.blade.php
@@ -94,7 +94,7 @@
 
 
                                             <div x-cloak x-show="isOpen" x-on:click.outside="isOpen=false"
-                                                class="z-auto origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
+                                                class="z-50 origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
                                                 role="menu" aria-orientation="vertical" aria-labelledby="menu-button"
                                                 tabindex="-1">
                                                 <div class="py-1 hover:bg-gray-100" role="none">
